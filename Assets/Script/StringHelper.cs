fb79f9ce4f35dc874db523664e13fb748a2a4ec2
StevenSawyer <sungyup10@naver.com>
2022-06-26 16:56:25 +0900

    유니티 기본프로젝트

---
+++ Assets/Script/StringHelper.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringHelper : MonoBehaviour
+{
+    #region ажа╕
+    public static readonly float TEST = 1f;
+    #endregion
+}
