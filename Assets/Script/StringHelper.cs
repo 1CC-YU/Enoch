d53f346b78c78e43162157a18d8a87fe2cf8a12a
StevenSawyer <sungyup10@naver.com>
2022-06-27 19:00:41 +0900

    Update StringHelper.cs

--- Assets/Script/StringHelper.cs
+++ Assets/Script/StringHelper.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class StringHelper : MonoBehaviour
 {
-    #region 주제
-    public static readonly float TEST = 1f;
+    #region 서식
+    public static readonly string FORMAT_AMOUNT = "x{0:n0}";
     #endregion
+
     #region 아이템
     public static readonly float BOOTS_ITEM_BUFF = 5f;
+    #endregion
+
+    #region 대화
+    public static readonly string MSG_PURCHASE = "{0}를 {1}g에 구매하였습니다.";
     #endregion
 }
