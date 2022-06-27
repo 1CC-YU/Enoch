939556eaa83295dc74238276e6f9a5a6abdd10b8
StevenSawyer <sungyup10@naver.com>
2022-06-27 18:40:38 +0900

    테스트 스크립트 수정

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float mSpeed;
+
+    private void Awake()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Script/StringHelper.cs
+++ Assets/Script/StringHelper.cs
@@ -7,4 +7,7 @@ public class StringHelper : MonoBehaviour
     #region 주제
     public static readonly float TEST = 1f;
     #endregion
+    #region 아이템
+    public static readonly float BOOTS_ITEM_BUFF = 5f;
+    #endregion
 }
