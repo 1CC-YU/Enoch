d232621d28e179d7715ad0ad7830e3327d191008
CuteRabbits <woejfl0824@naver.com>
2022-07-07 16:07:43 +0900

    SCRIPT
    PLAYER,
    StringHelper

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //스탯 =>[체력, 방어도, 숙련도, 힘, 운]
+    // => [health, depend, mastery, power, luck]
+    //채광 => [돌, 구리, 철, 루비, 다이아]
+    // => [stone, copper, steel, ruby, diamond]
+    
     [SerializeField]
     private float mSpeed;
+    private int health;
+    private int depend;
+    private int mastery;
+    private int power;
+    private int luck;
 
-    private void Awake()
+
+    void Update()
     {
-       
+        movePlayer();
     }
 
+    void movePlayer()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * mSpeed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+
+
+    void mining()
+    {
+        // 채광 종류 별로 태그(tag)로 줄것인지, 리스트화(enum) 시킬 것인지
+    }
+
+
+    void attack()
+    {
+
+    }
 }
--- Assets/Script/StringHelper.cs
+++ Assets/Script/StringHelper.cs
@@ -9,10 +9,83 @@ public class StringHelper : MonoBehaviour
     #endregion
 
     #region 아이템
-    public static readonly float BOOTS_ITEM_BUFF = 5f;
+    public static readonly int HEALTH_CARE_20 = 20;
+    public static readonly int HEALTH_CARE_50 = 50;
+    public static readonly int HEALTH_CARE_80 = 80;
+
+    public static readonly int HEALTH_PRICE_20 = 100;
+    public static readonly int HEALTH_RRICE_50 = 200;
+    public static readonly int HEALTH_PRICE_80 = 300;
     #endregion
 
     #region 대화
-    public static readonly string MSG_PURCHASE = "{0}를 {1}g에 구매하였습니다.";
+    public static readonly string MSG_PURCHASE = "{0}를 {1}Cos에 구매하였습니다.";
     #endregion
+
+
+    // 여기다 ENUM 써도 됨? 아니면 리스트, 그럼 LEVEL[1-10] 만들고오오
+    #region 스탯
+    // 숙련도 채광횟수
+    public static readonly int MASTERY_1_STONE = 5;
+    public static readonly int MASTERY_1_COPPER = 6;
+    public static readonly int MASTERY_1_STEEL = 7;
+
+    public static readonly int MASTERY_2_STONE = 5;
+    public static readonly int MASTERY_2_COPPER = 6;
+    public static readonly int MASTERY_2_STEEL = 7;
+
+    public static readonly int MASTERY_3_STONE = 4;
+    public static readonly int MASTERY_3_COPPER = 5;
+    public static readonly int MASTERY_3_STEEL = 6;
+
+    public static readonly int MASTERY_4_STONE = 4;
+    public static readonly int MASTERY_4_COPPER = 5;
+    public static readonly int MASTERY_4_STEEL = 6;
+
+    public static readonly int MASTERY_5_STONE = 3;
+    public static readonly int MASTERY_5_COPPER = 4;
+    public static readonly int MASTERY_5_STEEL = 5;
+    public static readonly int MASTERY_5_RUBY = 7;
+
+    public static readonly int MASTERY_6_STONE = 3;
+    public static readonly int MASTERY_6_COPPER = 4;
+    public static readonly int MASTERY_6_STEEL = 5;
+    public static readonly int MASTERY_6_RUBY = 6;
+
+    public static readonly int MASTERY_7_STONE = 2;
+    public static readonly int MASTERY_7_COPPER = 3;
+    public static readonly int MASTERY_7_STEEL = 4;
+    public static readonly int MASTERY_7_RUBY = 6;
+
+    public static readonly int MASTERY_8_STONE = 2;
+    public static readonly int MASTERY_8_COPPER = 3;
+    public static readonly int MASTERY_8_STEEL = 4;
+    public static readonly int MASTERY_8_RUBY = 5;
+    public static readonly int MASTERY_8_DIAMOND = 7;
+
+    public static readonly int MASTERY_9_STONE = 1;
+    public static readonly int MASTERY_9_COPPER = 2;
+    public static readonly int MASTERY_9_STEEL = 3;
+    public static readonly int MASTERY_9_RUBY = 5;
+    public static readonly int MASTERY_9_DIAMOND = 6;
+
+    public static readonly int MASTERY_10_STONE = 1;
+    public static readonly int MASTERY_10_COPPER = 2;
+    public static readonly int MASTERY_10_STEEL = 3;
+    public static readonly int MASTERY_10_RUBY = 4;
+    public static readonly int MASTERY_10_DIAMOND = 5;
+
+
+    // 운 광물획득량
+    public static readonly int LUCK_1_STONE = 1;
+    public static readonly int LUCK_1_COPPER = 1;
+    public static readonly int LUCK_1_STEEL = 1;
+    public static readonly int LUCK_1_RUBY = 1;
+    public static readonly int LUCK_1_DIAMOND = 1;
+
+    // 힘 아이템 소유 갯수
+
+
+    #endregion
+
 }
