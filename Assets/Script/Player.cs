e24f9b803d9e9b1af881a28552f4393e1195af5c
CuteRabbits <woejfl0824@naver.com>
2022-07-18 17:07:53 +0900

    player move
     수정

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,74 +11,70 @@ public class Player : MonoBehaviour
     
     [SerializeField]
     private float mSpeed;
-    private int health;
-    private int depend;
-    private int mastery;
-    private int power;
-    private int luck;
-    private Animator anim;
+    private int mHealth;
+    private int mDepend;
+    private int mMastery;
+    private int mPower;
+    private int mLuck;
+    private Animator mAnim;
 
-    void Awake()
+    private void Awake()
     {
-        anim = GetComponent<Animator>();
+        mAnim = GetComponent<Animator>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
         movePlayer();
     }
 
-    void movePlayer()
+    private void movePlayer()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 curPos = transform.position;
-        Vector3 nextPos = new Vector3(h, v, 0) * mSpeed * Time.deltaTime;
+        Vector3 nextPos = new Vector3(h, v, 0).normalized * mSpeed * Time.deltaTime;
 
         transform.position = curPos + nextPos;
 
-        float _dirX = Input.GetAxisRaw("Horizontal");
-        float _dirY = Input.GetAxisRaw("Vertical");
-
-        Vector3 direction = new Vector3(_dirX, _dirY);
-        anim.SetBool("Right", false);
-        anim.SetBool("Left", false);
-        anim.SetBool("Down", false);
-        anim.SetBool("Up", false);
+        Vector3 direction = new Vector3(h, v);
+        mAnim.SetBool("Right", false);
+        mAnim.SetBool("Left", false);
+        mAnim.SetBool("Down", false);
+        mAnim.SetBool("Up", false);
 
 
         if (direction != Vector3.zero)
         {
-            this.transform.Translate(direction.normalized * mSpeed * Time.deltaTime);
 
             if (direction.y > 0)
             {
-                anim.SetBool("Up", true);
+                mAnim.SetBool("Up", true);
             }
             else if (direction.y < 0)
             {
-                anim.SetBool("Down", true);
+                mAnim.SetBool("Down", true);
             }
             else if (direction.x > 0)
             {
-                anim.SetBool("Right", true);
+                mAnim.SetBool("Right", true);
             }
             else if (direction.x < 0)
             {
-                anim.SetBool("Left", true);
+                mAnim.SetBool("Left", true);
             }
             
         }
     }
 
 
-    void mining()
+    private void mining()
     {
         // 채광 종류 별로 태그(tag)로 줄것인지, 리스트화(enum) 시킬 것인지
     }
 
 
-    void attack()
+    private void attack()
     {
 
     }
