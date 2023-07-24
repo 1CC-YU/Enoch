5ba7ad96c0353a0a0d9c9ccfb5c4c07238c96a87
JagRyu <89447502+JagRyu@users.noreply.github.com>
2023-07-24 15:54:52 +0900

    Revert "Programming"

--- Assets/Script/ObjectManager.cs
+++ Assets/Script/ObjectManager.cs
@@ -4,49 +4,117 @@ using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
-    //enum으로 하던 배열로 하던 늘려서 구리, 루비 다 넣어야함.
-    GameObject itemGem;
+    GameObject[] stoneGem;
+    GameObject[] ironGem;
+    GameObject[] copperGem;
+    GameObject[] rubyGem;
+    GameObject[] diamondGem;
+    GameObject[] targetPool;
 
-    GameObject targetPool;
 
-    public GameObject itemGemProfab;
+    [SerializeField]
+    GameObject stoneGemPrefab;
+    [SerializeField]
+    GameObject ironGemPrefab;
+    [SerializeField]
+    GameObject copperGemPrefab;
+    [SerializeField]
+    GameObject rubyGemPrefab;
+    [SerializeField]
+    GameObject diamondGemPrefab;
+
+
 
     private void Awake()
     {
+        stoneGem = new GameObject[20];
+        ironGem = new GameObject[20];
+        copperGem = new GameObject[20];
+        rubyGem = new GameObject[20];
+        diamondGem = new GameObject[20];
+
         Generate();
     }
 
     private void Generate()
     {
-        
-            itemGem = Instantiate(itemGemProfab);
-            itemGem.SetActive(false);
-        
+        for(int index = 0; index < stoneGem.Length; index++)
+        {
+            stoneGem[index] = Instantiate(stoneGemPrefab);
+            stoneGem[index].SetActive(false);
+        }
+        for (int index = 0; index < ironGem.Length; index++)
+        {
+            ironGem[index] = Instantiate(ironGemPrefab);
+            ironGem[index].SetActive(false);
+        }
+        for (int index = 0; index < copperGem.Length; index++)
+        {
+            copperGem[index] = Instantiate(copperGemPrefab);
+            copperGem[index].SetActive(false);
+        }
+        for (int index = 0; index < rubyGem.Length; index++)
+        {
+            rubyGem[index] = Instantiate(rubyGemPrefab);
+            rubyGem[index].SetActive(false);
+        }
+        for (int index = 0; index < diamondGem.Length; index++)
+        {
+            diamondGem[index] = Instantiate(diamondGemPrefab);
+            diamondGem[index].SetActive(false);
+        }
+
     }
     public GameObject MakeObj(Stone_gem.MineralState type)
     {
         switch (type)
         {
             case Stone_gem.MineralState.Stone_gem:
-                targetPool = itemGem;
-                
+                targetPool = stoneGem;
+                break;
+            case Stone_gem.MineralState.Iron_gem:
+                targetPool = ironGem;
+                break;
+            case Stone_gem.MineralState.Copper_gem:
+                targetPool = copperGem;
+                break;
+            case Stone_gem.MineralState.Ruby_gem:
+                targetPool = rubyGem;
+                break;
+            case Stone_gem.MineralState.Diamond_gem:
+                targetPool = diamondGem;
                 break;
         }
-        if (!targetPool.activeSelf)
+        for(int index = 0; index<targetPool.Length; index++)
         {
-            targetPool.SetActive(true);
-            return targetPool;
+            if (!targetPool[index].activeSelf && targetPool[index]!= null)
+            {
+                targetPool[index].SetActive(true);
+                return targetPool[index];
+            }
         }
-
+        
         return null;
     }
 
-    public GameObject GetPool(Stone_gem.MineralState type)
+    public GameObject []GetPool(Stone_gem.MineralState type)
     {
         switch (type)
         {
             case Stone_gem.MineralState.Stone_gem:
-                targetPool = itemGem;
+                targetPool = stoneGem;
+                break;
+            case Stone_gem.MineralState.Iron_gem:
+                targetPool = ironGem;
+                break;
+            case Stone_gem.MineralState.Copper_gem:
+                targetPool = copperGem;
+                break;
+            case Stone_gem.MineralState.Ruby_gem:
+                targetPool = rubyGem;
+                break;
+            case Stone_gem.MineralState.Diamond_gem:
+                targetPool = diamondGem;
                 break;
         }
         return targetPool;
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// player를 움직이는 script
 public class Player : MonoBehaviour
 {
-    public float mSpeed;
-    public int mHealth;
-    public int mDepend;
-    public int mMastery;
-    public int mPower;
-    public int mLuck;
-    public int mLevel;
-    public int mExp;
+    public float mSpeed; //player 이동 속도
+    public int mHealth; //player 체력
+    public int mDepend; //player 방어도
+    public int mMastery; //player 숙련도
+    public int mPower; //player 힘
+    public int mLuck; //player 운
+    public int mLevel; //player 레벨
+    public int mExp; //player 경험치
+    public int mMoney; //player 돈
 
     private float mCurrTime;
 
     private Animator mAnim;
+    private Animation mAnimation;
     [SerializeField]
     private SaveSaver mSave;
     [SerializeField]
     private DBManager mDBManager;
     [SerializeField]
     private GameObject mHitZone;
-    
-    GameObject nearobjet;
+
+    GameObject nearobject;
+
 
     public float horizontal;
     public float vertical;
@@ -33,8 +37,7 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         mAnim = GetComponent<Animator>();
-
-        //Save만 해야하는지 Load & Save 해야하는지..
+        mAnimation = GetComponent<Animation>();
         StartCoroutine(Load());
         StartCoroutine(Save());
     }
@@ -42,20 +45,23 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         mCurrTime += Time.deltaTime;
-        if (mCurrTime > 300)
+        if (mCurrTime > 3)
         {
             StartCoroutine(Save());
             StartCoroutine(Load());
             mCurrTime = 0;
         }
     }
-    
+
     private void Update()
     {
         getInput();
         movePlayer();
         pickupItem();
     }
+    //by으니, getInput() - 220825
+    //Input.Get~ 는 한곳에 모아두는게 좋을 것 같다고 판단.
+    //한곳에 모아두는 메소드
     private void getInput()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -63,9 +69,12 @@ public class Player : MonoBehaviour
         swing = Input.GetButtonDown("Jump");
         pickup = Input.GetButtonDown("Pickup");
     }
+
+    //by으니, movePlayer() - 220810
+    //Player 움직임 구현
+    //키보드를 이용하여 움직임
     private void movePlayer()
     {
-
         Vector3 curPos = transform.position;
         Vector3 nextPos = new Vector3(horizontal, vertical, 0).normalized * mSpeed * Time.deltaTime;
         transform.position = curPos + nextPos;
@@ -76,53 +85,67 @@ public class Player : MonoBehaviour
         mAnim.SetBool("Down", false);
         mAnim.SetBool("Up", false);
 
-
         if (direction != Vector3.zero)
         {
-
             if (direction.y > 0)
             {
                 mAnim.SetBool("Up", true);
                 mHitZone.transform.position = new Vector3(transform.position.x + (-0.02f), transform.position.y + 0.75f, 0);
-                
             }
             else if (direction.y < 0)
             {
                 mAnim.SetBool("Down", true);
                 mHitZone.transform.position = new Vector3(transform.position.x + (-0.02f), transform.position.y + (-0.75f), 0);
-
-
             }
             else if (direction.x > 0)
             {
                 mAnim.SetBool("Right", true);
                 mHitZone.transform.position = new Vector3(transform.position.x + 0.5f, transform.position.y + (-0.2f), 0);
-
-
             }
             else if (direction.x < 0)
             {
                 mAnim.SetBool("Left", true);
                 mHitZone.transform.position = new Vector3(transform.position.x + (-0.5f), transform.position.y + (-0.2f), 0);
-
-
             }
-
         }
-
         if (swing)
         {
             mAnim.SetTrigger("doMining");
         }
-
     }
+
+    
+
+    private void OnAttack(Transform enemy)
+    {
+        //damaged
+    }
+
+    private void OnDie()
+    {
+        if (mHealth > 0)
+        {
+            //Damaged
+        }
+        else if(mHealth <= 0)
+        {
+            //die
+        }
+    }
+
+    private void OnDamaged(Vector2 targetPos)
+    {
+        //맞는 애니메이션이랑 죽는 모션 없음
+    }
+
+    //by으니, pickupItem() - 220905
     private void pickupItem()
     {
-        if (pickup && nearobjet != null)
+        if (pickup && nearobject != null)
         {
-            if (nearobjet.tag == "Gem")
+            if (nearobject.tag == "Gem")
             {
-                Destroy(nearobjet);
+                Destroy(nearobject);
             }
         }
     }
@@ -132,31 +155,30 @@ public class Player : MonoBehaviour
     {
         if (collision.gameObject.tag == "Monster")
         {
-           
+
         }
-        
     }
 
+    //by재그, 드롭된 아이템 먹기
+    //gem 먹기
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Gem")
         {
-
-            nearobjet = collision.gameObject;
+            nearobject = collision.gameObject;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Gem")
+        if (collision.gameObject.tag == "Gem")
         {
-
-            nearobjet = null;
+            nearobject = null;
         }
     }
 
-  
-
-
+    //by재그, Save() && Load() 구현 - 220831
+    //Player 스탯들을 저장&로드
+    //5분마다 자동 저장
     IEnumerator Save()
     {
         mSave.Saver();
